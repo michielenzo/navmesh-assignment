0dca402693694fd12b0c5d981d78b34e33594a60
Michiel Idema <m.idema@textinfo.nl>
2020-03-28 22:09:25 +0100

    commit

--- Assets/Scripts/MazeGeneration.cs
+++ Assets/Scripts/MazeGeneration.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
-using Vector2 = System.Numerics.Vector2;
 
 public class MazeGeneration : MonoBehaviour
 {
