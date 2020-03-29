7d7ab8d15c826997c2f7672ba14a5c59d75f5f2a
michielenzo <michielidema@outlook.com>
2020-03-29 20:43:35 +0200

    done

--- Assets/Scripts/MazeGeneration.cs
+++ Assets/Scripts/MazeGeneration.cs
@@ -9,54 +9,62 @@ public class MazeGeneration : MonoBehaviour
 
     public GameObject player;
 
-    private bool _playerSpawned = false;
+    private const float _wallSpawnChange = 0.2f;
 
+    private bool _playerSpawned;
+    
     private readonly Vector3 _wallOffset = new Vector3(4.5f,-0.5f,4.5f);
     private readonly Vector3 _playerOffset = new Vector3(4.5f,0.7f,4.5f);
     
     public void Start()
     {
-        InstantiatePlayer(planes[0]);
+        //Two foreaches are used because otherwise the NavMeshSurfaces will not bake correctly.
         foreach (var plane in planes) {
             GenerateMaze(plane);
-            NavMeshSurface navMeshSurface = plane.GetComponent(typeof(NavMeshSurface)) as NavMeshSurface;
-            if (navMeshSurface != null)
-            {
-                navMeshSurface.BuildNavMesh();
-            }
-                
         }
-        
+
+        foreach (var plane in planes) {
+            BakeNavigationMesh(plane);          
+        }
     }
 
     private void GenerateMaze(GameObject plane)
     {
         const int numCols = 10;
         const int numRows = 10;
-        
-        var planeGrid = new int[numCols, numRows];
 
         for (int col = 0; col < numCols; col++) {
             for (int row = 0; row < numRows; row++) {
-                var rand = Random.Range(0.0f, 1.0f);
-                if (!(Random.Range(0.0f, 1.0f) <= 0.3f)) continue;
-                if (plane.transform.name == "Plane1" && row == 10 && col == 10) continue;
-                InstantiateWall(plane, col, row);
+                if (!_playerSpawned) {
+                    InstantiatePlayer(plane, col, row);
+                } else {
+                    float diceRoll = Random.Range(0.0f, 1.0f);
+                    if (!(diceRoll <= _wallSpawnChange)) continue;
+                    InstantiateWall(plane, col, row);                    
+                }
             }
         }
+    }
+    
+    private static void BakeNavigationMesh(GameObject plane) 
+    {
+        NavMeshSurface navMeshSurface = plane.GetComponent(typeof(NavMeshSurface)) as NavMeshSurface;
+        if (navMeshSurface != null) navMeshSurface.BuildNavMesh();
     }
 
     private void InstantiateWall(GameObject plane, int col, int row)
     {
         var wallOffset = new Vector3(col, 0, row);
-        var wallPosition = wallOffset + plane.transform.position - this._wallOffset;
+        var wallPosition = wallOffset + plane.transform.position - _wallOffset;
         var wallGameObject = Instantiate(wall, wallPosition, Quaternion.identity);
         wallGameObject.transform.SetParent(plane.transform);
     }
 
-    private void InstantiatePlayer(GameObject plane)
+    private void InstantiatePlayer(GameObject plane, int col, int row)
     {
-        var playerPosition = plane.transform.position + _playerOffset;
+        var wallOffset = new Vector3(col, 0, row);
+        var playerPosition = plane.transform.position + _playerOffset + wallOffset;
         Instantiate(player, playerPosition, Quaternion.identity);
+        _playerSpawned = true;
     }
 }
